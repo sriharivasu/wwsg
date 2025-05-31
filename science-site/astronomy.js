36d5191ed428cf21f6a786b5e29725f1268a669e
Harini Vasudevan <harini.vasudevan@oracle.com>
2025-05-31 13:10:04 -0400

    Many changes...

--- science-site/astronomy.js
+++ science-site/astronomy.js
@@ -1,15 +1,12 @@
-
+let activeButton = null;
 
 function w3_open() {
-
     document.getElementById("sidebar").style.width = "25%";
     document.getElementById("sidebar").style.display = "";
     document.getElementById("main").style.marginLeft = "25%";
     document.getElementById("header").style.marginLeft = "20%";
     document.getElementById("header").style.width = "80%";
     document.getElementById("openNav").style.display = "none";
-
-
 }
 
 function w3_close() {
@@ -22,6 +19,25 @@ function w3_close() {
 }
 
 function showChildren(targetId) {
+    const clickedButton = event.target.closest('.person.has-children');
+    const buttons = document.querySelectorAll('.person.has-children');
+
+    clickedButton.style.filter = "opacity(100%) sepia(0%)";
+    clickedButton.style.borderColor = "#007bff";
+    clickedButton.style.boxShadow = "0 8px 25px rgba(0, 123, 255, 0.2)";
+    clickedButton.style.background = "rgb(232, 239, 255)"; // Light blue background for active button
+    clickedButton.style.fontStyle = "oblique";
+    buttons.forEach(button => {
+        if (button !== clickedButton) {
+            button.style.filter = "opacity(90%) sepia(30%)";
+            button.style.border = "2px solid blue";
+            button.style.boxShadow = "0 0 10px rgba(0, 0, 255, 0.5)";
+            button.style.fontStyle = "normal";
+
+        }
+    });
+    activeButton = clickedButton;
+
     // Get the generation number from the target ID
     const genNumber = targetId.split('-')[1];
 
@@ -53,4 +69,14 @@ function showChildren(targetId) {
             target.scrollIntoView({ behavior: 'smooth', block: 'center' });
         }, 100);
     }
+}
+
+function toggleHighlight(button) {
+    if (activeButton) {
+        activeButton.classList.remove('active');
+    }
+
+    // Add highlight to clicked button
+    button.classList.add('active');
+    activeButton = button;
 }
